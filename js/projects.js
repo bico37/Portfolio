2c3ad639952e9a3ade739360109e30456b6fe917
Erik <bergi.rik@gmail.com>
2024-11-04 21:38:34 +0100

    created project files

---
+++ js/projects.js
@@ -0,0 +1,20 @@
+// onlick events for the kinds of projects
+// add event listener for #web-development
+document.getElementById('web-development').addEventListener('click', function() {
+    location.href = 'web-development.html';
+});
+// add event listener for #photography
+document.getElementById('photography').addEventListener('click', function() {
+    location.href = 'photography.html';
+}
+);
+// add event listener for #programming
+document.getElementById('programming').addEventListener('click', function() {
+    location.href = 'programming.html';
+}
+);
+// add event listener for #videos
+document.getElementById('videos').addEventListener('click', function() {
+    location.href = 'videos.html';
+}
+);
