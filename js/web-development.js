bad4e5f81e82f2078533e5c0db881cf7a3715efa
Erik Bergmair <e.bergmair@HTBLALeonding.onmicrosoft.com>
2024-11-19 23:02:51 +0100

    Added Web Development design and placeholder, searched up my projects

---
+++ js/web-development.js
@@ -0,0 +1,10 @@
+// Projects Opjects
+
+let adventskalender = {
+    video: "./Pic/Projects/Adventskalender.jpg",
+    link: "https://test/",
+    name: "Adventskalender",
+    description: "An Advent Kaleneder with fifa players",
+    date: "Nov. 2024",
+    tech: "HTML, CSS, JS, PHP"
+}
