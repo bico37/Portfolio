8d9d402374153c6bf60ee7fc5bdb1bfba69875b6
Erik <bergi.rik@gmail.com>
2024-11-23 22:27:52 +0100

    finished web projects overview

--- js/web-development.js
+++ js/web-development.js
@@ -1,10 +1,285 @@
-// Projects Opjects
+// 1 klasse
+let formula1 = {
+  ranking: 0,
+  cover: "formula1.png",
+  video: "formula1.mp4",
+  link: "https://placeholder/",
+  name: "Formula 1",
+  description: "A project displaying a Formula 1 webpage layout.",
+  date: "March - June 2023",
+  tech: "HTML, CSS",
+};
 
-let adventskalender = {
-    video: "./Pic/Projects/Adventskalender.jpg",
-    link: "https://test/",
-    name: "Adventskalender",
-    description: "An Advent Kaleneder with fifa players",
-    date: "Nov. 2024",
-    tech: "HTML, CSS, JS, PHP"
+// 2 klasse
+let carsFullScreen = {
+  ranking: 0,
+  cover: "carsFullScreen.png",
+  video: "carsFullScreen.mp4",
+  link: "https://placeholder/",
+  name: "Cars Full Screen",
+  description: "A project displaying a full-screen layout with HTML and CSS.",
+  date: "September 2023",
+  tech: "HTML, CSS",
+};
+
+let diceAttack = {
+  ranking: 0,
+  cover: "diceAttack.png",
+  video: "diceAttack.mp4",
+  link: "https://placeholder/",
+  name: "Dice Attack",
+  description:
+    "A project where JS was used for the first time to create an interactive dice game.",
+  date: "October 2023",
+  tech: "HTML, CSS, JS",
+};
+
+let ticTacToe = {
+  ranking: 0,
+  cover: "ticTacToe.png",
+  video: "ticTacToe.mp4",
+  link: "https://placeholder/",
+  name: "TicTacToe",
+  description: "A classic Tic Tac Toe game built with JavaScript.",
+  date: "November 2023",
+  tech: "HTML, CSS, JS",
+};
+
+let formula1Driver = {
+  ranking: 0,
+  cover: "formula1Driver.png",
+  video: "formula1Driver.mp4",
+  link: "https://placeholder/",
+  name: "Formula 1 Driver",
+  description: "An interactive webpage focused on Formula 1 drivers.",
+  date: "December 2023",
+  tech: "HTML, CSS, JS",
+};
+
+let weatherAPI = {
+  ranking: 0,
+  cover: "weatherAPI.png",
+  video: "weatherAPI.mp4",
+  link: "https://placeholder/",
+  name: "Weather API",
+  description: "A project using APIs to fetch and display weather data.",
+  date: "November 2023",
+  tech: "HTML, CSS, JS, API",
+};
+
+let foodAPI = {
+  ranking: 0,
+  cover: "foodAPI.png",
+  video: "foodAPI.mp4",
+  link: "https://placeholder/",
+  name: "Food API",
+  description: "An application that uses APIs to display food-related data.",
+  date: "November 2023",
+  tech: "HTML, CSS, JS, API",
+};
+
+let valorantAPI = {
+  ranking: 0,
+  cover: "valorantAPI.png",
+  video: "valorantAPI.mp4",
+  link: "https://placeholder/",
+  name: "Valorant API",
+  description: "An API-based project showcasing data about Valorant.",
+  date: "November 2023",
+  tech: "HTML, CSS, JS, API",
+};
+
+// 2024
+let austrianSkier = {
+  ranking: 0,
+  cover: "austrianSkier.png",
+  video: "austrianSkier.mp4",
+  link: "https://placeholder/",
+  name: "Austrian Skier",
+  description:
+    "A project developed with TypeScript, NodeJS, and ExpressJS featuring Austrian skiers.",
+  date: "January 2024",
+  tech: "HTML, CSS, JS, TS, NodeJS, ExpressJS",
+};
+
+let todoList = {
+  ranking: 0,
+  cover: "todoList.png",
+  video: "todoList.mp4",
+  link: "https://placeholder/",
+  name: "TODO List",
+  description: "A to-do list application using TypeScript and NodeJS.",
+  date: "February 2024",
+  tech: "HTML, CSS, JS, TS, NodeJS, ExpressJS",
+};
+
+let drinkWaterApp = {
+  ranking: 1,
+  cover: "aquaMate.jpg",
+  video: "aquaMate.mp4",
+  link: "https://placeholder/",
+  name: "Aqua Mate",
+  description: "A smartphone app to track water consumption.",
+  date: "March 2024",
+  tech: "HTML, CSS, JS, TS, NodeJS, ExpressJS",
+};
+
+let snowman = {
+  ranking: 1,
+  cover: "snowman.png",
+  video: "snowman.mp4",
+  link: "https://placeholder/",
+  name: "Babylon Snowman",
+  description: "A 3D snowman created using BabylonJS.",
+  date: "April 2024",
+  tech: "HTML, CSS, JS, TS, BabylonJS",
+};
+
+let babylon3DGame = {
+  ranking: 0,
+  cover: "babylon3DGame.png",
+  video: "babylon3DGame.mp4",
+  link: "https://placeholder/",
+  name: "Babylon 3D Game",
+  description: "A 3D game developed with BabylonJS.",
+  date: "May 2024",
+  tech: "HTML, CSS, JS, TS, BabylonJS",
+};
+
+let soccer2DGame = {
+  ranking: 0,
+  cover: "soccer2DGame.png",
+  video: "soccer2DGame.mp4",
+  link: "https://placeholder/",
+  name: "Soccer 2D Game",
+  description: "A 2D soccer game.",
+  date: "January - March 2024",
+  tech: "HTML, CSS, JS",
+};
+
+let travelPilot = {
+  ranking: 0,
+  cover: "travelPilot.png",
+  video: "travelPilot.mp4",
+  link: "https://placeholder/",
+  name: "Travel Pilot",
+  description: "A project showcasing a travel agency website.",
+  date: "March - June 2024",
+  tech: "HTML, CSS, JS",
+};
+
+// 3 klasse
+let tshirtdesigner = {
+  ranking: 1,
+  cover: "customClothery.png",
+  video: "customClothery.mp4",
+  link: "https://placeholder/",
+  name: "CustomClothery",
+  description:
+    "A project allowing users to design custom t-shirts and hoodies.",
+  date: "September 2024",
+  tech: "HTML, CSS, JS",
+};
+
+let adventCalendar = {
+  ranking: 1,
+  cover: "adventCalendar.png",
+  video: "adventCalendar.mp4",
+  link: "https://placeholder/",
+  name: "Advent Calendar",
+  description: "An advent calendar featuring FIFA players.",
+  date: "November 2024",
+  tech: "HTML, CSS, JS, PHP",
+};
+
+let spotlas = {
+  ranking: 1,
+  cover: "spotlas.png",
+  video: "spotlas.mp4",
+  link: "https://placeholder/",
+  name: "Spotlas",
+  description:
+    "A platform showcasing hidden gems and unique places, designed to foster a community for sharing discoveries. Developed by a 4-person team with me as the project leader and backend developer .",
+  date: "November 2024 - June 2025",
+  tech: "HTML, CSS, Bootsrap, JS, PHP, MariaDB ",
+};
+
+let projects = [
+  formula1,
+  carsFullScreen,
+  diceAttack,
+  ticTacToe,
+  formula1Driver,
+  weatherAPI,
+  foodAPI,
+  valorantAPI,
+  austrianSkier,
+  // todoList,
+  drinkWaterApp,
+  snowman,
+  // babylon3DGame,
+  tshirtdesigner,
+  adventCalendar,
+  spotlas,
+  soccer2DGame,
+  travelPilot,
+];
+
+// sort projects by date
+// Funktion zur Umwandlung des Datums in ein standardisiertes Format (yyyy-mm)
+function parseDate(dateStr) {
+  const dateRange = dateStr.split(" - "); // Teilt bei Zeiträumen in Start und Ende
+  const dateParts = (dateRange[1] || dateRange[0]).split(" "); // Nimmt das Ende, falls vorhanden
+  const months = {
+    January: "01",
+    February: "02",
+    March: "03",
+    April: "04",
+    May: "05",
+    June: "06",
+    July: "07",
+    August: "08",
+    September: "09",
+    October: "10",
+    November: "11",
+    December: "12",
+  };
+
+  return `${dateParts[1]}-${months[dateParts[0]]}`; // yyyy-mm
 }
+
+// Projekte nach Datum sortieren (neueste zuerst)
+projects.sort((a, b) => {
+  const dateA = parseDate(a.date);
+  const dateB = parseDate(b.date);
+
+  return dateB.localeCompare(dateA); // Neueste zuerst
+});
+
+// Sortierte Projekte ausgeben
+console.log(projects);
+
+// print projects
+function printProjectOverview() {
+  let contentString = "";
+
+  for (let i = 0; i < projects.length; i++) {
+    let project = projects[i];
+    contentString += `
+        <div class="project">
+            <div class="project-img">
+                <img src="../assets/img/projects/webDev/cover/${project.cover}" alt="" />
+            </div>
+            <div class="project-text">
+                <h2>${project.name}</h2>
+                <p>${project.date}</p>
+                <p class="technologies">${project.tech}</p>
+            </div>
+        </div>
+        `;
+  }
+
+  document.getElementById("content-wrapper").innerHTML = contentString;
+}
+
+printProjectOverview();
